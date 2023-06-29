3eed27fdb83991a6a33f63c62b1aeacf3b4861f0
NehaP0 <nehaphadtare334@gmail.com>
2023-06-29 22:43:47 +0530

    jokes

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [keyword, setKeyword] = useState('');
+  const [joke, setJoke] = useState('');
+
+  const handleKeywordChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const handleGenerateJoke = () => {
+    fetch(`http://localhost:8080/jokes?keyword=${keyword}`)
+      .then((response) => response.json())
+      .then((data) => setJoke(data.joke))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="container">
+      <div className="background"></div>
+      <div className="content">
+        <h1>Joke Generator</h1>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Enter a keyword..."
+            value={keyword}
+            onChange={handleKeywordChange}
+          />
+          <button onClick={handleGenerateJoke}>Generate Joke</button>
+        </div>
+        {joke && (
+          <div className="joke-container">
+            <h2>Generated Joke:</h2>
+            <p>{joke}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
